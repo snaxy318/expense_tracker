48ed96570785e01530e3201cc763f814085d50e9
snaxy318 <suryanshtejas@gmail.com>
2024-11-01 21:54:34 +0530

    added rows to Items

--- lib/widgets/expenses_list/expenses_item.dart
+++ lib/widgets/expenses_list/expenses_item.dart
@@ -2,21 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
 
 class ExpensesItem extends StatelessWidget {
-  const ExpensesItem(this.expense,{super.key});
+  const ExpensesItem(this.expense, {super.key});
 
   final Expense expense;
 
   @override
-  Widget build(BuildContext context){
-    return Padding(
-      padding:(
-        const EdgeInsets.symmetric(
-          horizontal: 20,
-          vertical: 16
-        )
-      ),
-      child: Card(
-        child: Text(expense.title),
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: (const EdgeInsets.symmetric(horizontal: 20, vertical: 16)),
+        child: Column(
+          children: [
+            Text(expense.title),
+            const SizedBox(height: 4),
+            Row(
+              children: [
+                Text('\$${expense.amount.toStringAsFixed(2)}'), //12.33 => 12.33
+                const Spacer(),
+                Row(
+                  children: [
+                    const Icon(Icons.abc),
+                    const SizedBox(width: 8),
+                    Text(expense.date.toString()),
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
