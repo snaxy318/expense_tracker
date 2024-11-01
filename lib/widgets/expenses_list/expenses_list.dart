b3b280e5b51cc54f2366291a69d3e153b1da10c8
snaxy318 <suryanshtejas@gmail.com>
2024-11-01 21:35:03 +0530

    added Card Wiget to it

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -1,5 +1,6 @@
+import 'package:expense_tracker/widgets/expenses_list/expenses_item.dart';
 import 'package:flutter/material.dart';
-import 'models/expense.dart';
+import '../../models/expense.dart';
 
 class ExpensesList extends StatelessWidget {
   const ExpensesList({
@@ -13,7 +14,7 @@ class ExpensesList extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (ctx, index) => Text(expenses[index].title),
+      itemBuilder: (ctx, index) => ExpensesItem(expenses[index]),
     );
   }
 }
