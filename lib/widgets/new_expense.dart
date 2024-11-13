0459e7e86a6bd9a3b07cbfd47c12f69b3151297d
snaxy318 <suryanshtejas@gmail.com>
2024-11-13 18:50:46 +0530

    check if information is correct as provided

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -34,7 +34,11 @@ class _NewExpenseState extends State<NewExpense> {
   }
 
   void _submitExpenseData() {
-
+    final enteredAmount = double.tryParse(_amountController.text);
+    final amountIsInvalid = (enteredAmount == null || enteredAmount <= 0);
+    if(_titleController.text.trim().isEmpty || amountIsInvalid || _selectedDate == null){
+      //show error message 
+    }
   }
 
   @override
