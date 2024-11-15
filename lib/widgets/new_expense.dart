a5f27e8c8a062f46246dba4febacc90a1b2f4747
snaxy318 <suryanshtejas@gmail.com>
2024-11-15 12:12:24 +0530

    closing Overlay after saving expense

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -67,6 +67,7 @@ class _NewExpenseState extends State<NewExpense> {
           date: _selectedDate!,
           category: _selectedCategory),
     );
+    Navigator.pop(context);
   }
 
   @override
