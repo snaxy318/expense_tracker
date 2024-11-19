d27244bc5547958b58f407918e0761a16091c4ce
snaxy318 <suryanshtejas@gmail.com>
2024-11-19 23:28:00 +0530

    added cardTheme

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,13 @@ void main() {
           backgroundColor: kColorScheme.onPrimaryContainer,
           foregroundColor: kColorScheme.primaryContainer,
         ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8
+          )
+        )
       ),
       home: const Expenses(),
     ),
