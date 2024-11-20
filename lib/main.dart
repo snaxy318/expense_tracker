2cb1944167f089638605322b55b1c3ea0c70e982
snaxy318 <suryanshtejas@gmail.com>
2024-11-20 23:46:30 +0530

    added theme to expense_item

--- lib/main.dart
+++ lib/main.dart
@@ -28,9 +28,9 @@ void main() {
         ),
         textTheme: ThemeData().textTheme.copyWith(
           titleLarge: TextStyle(
-            fontWeight: FontWeight.normal,
+            fontWeight: FontWeight.bold,
             color: kColorScheme.onSecondaryContainer,
-            fontSize: 28,
+            fontSize: 17,
           )
         )
       ),
