b22c7ef641f34ef614b83d1380428c205a8f6540
snaxy318 <suryanshtejas@gmail.com>
2024-11-21 00:05:44 +0530

    added background to Dismissal

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -18,6 +18,10 @@ class ExpensesList extends StatelessWidget {
         itemCount: expenses.length,
         itemBuilder: (ctx, index) => Dismissible(
               key: ValueKey(expenses[index]),
+              background: Container(
+                color: Theme.of(context).colorScheme.error.withGreen(125),
+                margin: EdgeInsets.symmetric(horizontal: Theme.of(context).cardTheme.margin!.horizontal)
+              ),
               onDismissed: (direction) {
                 onRemoveExpense(expenses[index]);
               },
